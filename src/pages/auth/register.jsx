1b3fe23c35c580395b26ba2ea5f7a4e41e026646
alimh17 <amohamadi17@gmail.com>
2023-06-26 15:43:15 +0330

    register page

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,11 +1,45 @@
+"use client";
 import Link from "next/link";
+
+import {
+  Container,
+  FormControl,
+  Text,
+  Input,
+  Button,
+  Heading,
+  Checkbox,
+} from "@chakra-ui/react";
 
 const Register = () => {
   return (
-    <div>
-      Register
-      <Link href="/auth/login">Login</Link>
-    </div>
+    <Container
+      maxW={990}
+      h="100vh"
+      display="flex"
+      flexFlow="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Heading as="h2" size="xl" my={8}>
+        Dev Mentor
+      </Heading>
+      <FormControl w={{ base: "auto", md: "md" }}>
+        <Input my={2} placeholder="نام" />
+        <Input my={2} placeholder="نام خانوادگی" />
+        <Input my={2} placeholder="ایمیل" />
+        <Input my={2} placeholder="کلمه عبور" />
+        <Checkbox my={2}>ظوابط و قوانین را می پذیرم</Checkbox>
+        <br />
+        <Button my={2} colorScheme="blue" w="100%">
+          <Link href="/auth/login">Login</Link>
+        </Button>
+        <Text textAlign="right" w="100%" as="span" mx="2">
+          حساب کاربری دارید؟
+        </Text>
+        <Link href="/auth/login">ورود</Link>
+      </FormControl>
+    </Container>
   );
 };
 
