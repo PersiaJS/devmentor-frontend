2a66b9b0c8ddabf89eadd526fc405cd035afdae4
alimh17 <amohamadi17@gmail.com>
2023-06-28 16:43:54 +0330

    added mentor-card

--- src/components/MentorCard/index.jsx
+++ src/components/MentorCard/index.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FaStar } from "react-icons/fa";
-import { BsChat, BsTelephone } from "react-icons/bs";
 
 import {
   Box,
@@ -15,15 +13,17 @@ import {
   Divider,
   Button,
 } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
+import { BsChat, BsPhone, BsTelephone } from "react-icons/bs";
 
 const MentorCard = () => {
   return (
     <Card
       sx={{
+        w: "sm",
         border: "1px solid #eaeaea",
         boxShadow: "sm",
       }}
-      w={{ base: "xs", lg: "sm" }}
     >
       <CardBody>
         <Box
@@ -34,7 +34,7 @@ const MentorCard = () => {
           <Image
             sx={{
               w: "full",
-              h: "15rem",
+              h: "20rem",
               borderRadius: "10px",
             }}
             src="mentor.jpg"
@@ -59,11 +59,31 @@ const MentorCard = () => {
             <Text>5.0</Text>
             <FaStar color="gold" />
           </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              paddingY: "2px",
+              paddingX: "2px",
+              borderRadius: "10px",
+              w: "full",
+              bg: "#2b6db0",
+              h: "6rem",
+              opacity: ".5",
+              bottom: "0",
+              maskImage:
+                "linear-gradient(to top, #ffffff 70%, transparent 100%)",
+              color: "#fff",
+              display: "flex",
+              flexFlow: "column",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Text mx="2" fontWeight="bold">
+              کری ریتر
+            </Text>
+            <Text mx="2">مهندس نرم افزار در مایکروسافت</Text>
+          </Box>
         </Box>
-        <Text mx="2" mt={2} fontWeight="bold">
-          کری ریتر
-        </Text>
-        <Text mx="2">مهندس نرم افزار در مایکروسافت</Text>
         <Text fontSize="sm" p="2">
           من به مهندسان کمک می کنم تا محصولات عالی بسازند و ارسال کنند. چه
           کارآفرینی باشید که تلاش می‌کند درخواست خود را به خط پایان برساند یا یک
@@ -90,24 +110,24 @@ const MentorCard = () => {
         <Divider />
         <Box>
           <Flex p="2" alignItems="center">
-            <Text fontSize="2xl" fontWeight="bold">
+            <Text fontSize="2xl" fontWeght="bold">
               240 دلار
             </Text>
             <Text> / ماه </Text>
           </Flex>
           <Flex p="2" alignItems="center" gap="4">
             <HStack fontSize="sm" color="gray">
-              <BsChat />
               <Text>گفتگو</Text>
+              <BsChat />
             </HStack>
             <HStack fontSize="sm" color="gray">
-              <BsTelephone />
               <Text>تماس</Text>
+              <BsTelephone />
             </HStack>
           </Flex>
         </Box>
         <Divider />
-        <Button w="full" colorScheme="red" my="2">
+        <Button w="full" colorScheme="blue" my="2">
           پروفایل
         </Button>
       </CardBody>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout/Layout";
-import MiniCard from "@/components/MiniCard/MiniCard";
-import { Box } from "@chakra-ui/react";
+import Hero from "@/components/Hero/Hero";
+import { Card } from "@chakra-ui/react";
 import Head from "next/head";
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import Link from "next/link";
+
 import MentorCard from "@/components/MentorCard";
 import { Flex } from "@chakra-ui/react";
+import ResultSearchMentor from "@/components/ResultSearchMentor";
 
 export default function Home() {
   return (
@@ -16,21 +18,41 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        {/* <main>
+        <div>
           <Link href="/auth/register">Register</Link>
           <Link href="/auth/login">Login</Link>
           <Link href="/auth/reset">Reset</Link>
           <Link href="/auth/forget">Forget</Link>
           <Link href="/auth/verify">Verify</Link>
-        </main> */}
-        <Box
-          display={"flex"}
-          flexDir={"row"}
-          flexWrap={"wrap"}
-          justifyContent={"space-evenly"}
-        >
-          <MiniCard />
-        </Box>
+          <Hero
+            title="مربی خودتو پیدا کن"
+            description="برای تسلط بر جاوا اسکریپت تنهایی؟ از منتور های ما کمک بگیر و چیزای خارق‌العاده خلق کن."
+            textOfMentorLogo="منتورهاتو پیدا کن"
+          />
+          <Box
+            display={"flex"}
+            flexDir={"row"}
+            flexWrap={"wrap"}
+            justifyContent={"space-evenly"}
+          >
+            <Box margin={5}>
+              <MiniCard />
+            </Box>
+            <Box margin={5}>
+              <MiniCard />
+            </Box>
+            <Box margin={5}>
+              <MiniCard />
+            </Box>
+          </Box>
+
+          <Flex gap="2">
+            <MentorCard />
+            <MentorCard />
+            <MentorCard />
+          </Flex>
+          <ResultSearchMentor />
+        </div>
       </Layout>
     </>
   );
