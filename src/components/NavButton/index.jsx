89d4666f6faf7b435c942d317b1de6896b075111
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-09-08 14:23:21 +0330

    feat: add role-based authorization custom hook

--- src/components/NavButton/index.jsx
+++ src/components/NavButton/index.jsx
@@ -1,13 +1,15 @@
 import { Button, HStack, Icon, Text } from "@chakra-ui/react";
 
 export const NavButton = (props) => {
-  const { icon, label, ...buttonProps } = props;
+  const { icon, url, label, ...buttonProps } = props;
   return (
     <Button
+      as="a"
       variant="ghost"
       textColor="gray.600"
       justifyContent="start"
       {...buttonProps}
+      href={url}
     >
       <HStack spacing="3">
         <Icon as={icon} boxSize="6" color="subtle" />
